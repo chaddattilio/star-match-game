b84028f510c747deeee85cc26fd851ab76cc4093
Chad Dattilio <cdattilio@lyonscg.com>
2019-03-10 18:42:38 -0400

    Initial commit of files

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { render } from "react-dom";
+import "./style.scss";
+
+const StarMatch = () => {
+  return (
+    <div className="game">
+      <div className="help">
+        Pick 1 or more numbers that sum to the number of stars
+      </div>
+      <div className="body">
+        <div className="left">
+          <div className="star" />
+          <div className="star" />
+          <div className="star" />
+          <div className="star" />
+          <div className="star" />
+          <div className="star" />
+          <div className="star" />
+          <div className="star" />
+          <div className="star" />
+        </div>
+        <div className="right">
+          <button className="number">1</button>
+          <button className="number">2</button>
+          <button className="number">3</button>
+          <button className="number">4</button>
+          <button className="number">5</button>
+          <button className="number">6</button>
+          <button className="number">7</button>
+          <button className="number">8</button>
+          <button className="number">9</button>
+        </div>
+      </div>
+      <div className="timer">Time Remaining: 10</div>
+    </div>
+  );
+};
+
+render(<StarMatch />, document.getElementById("root"));
